0bdc66d7f1feedad290c7d845f358a5c70c2be23
kaatreHx <helloworld00ff@gmail.com>
2026-09-08 22:16:19 +0545

    Initial commit

---
+++ ChatModels/chatmodel_demo.py
@@ -0,0 +1,22 @@
+# from langchain_openai import ChatOpenAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# model = ChatOpenAI(model='gpt-6-astra')
+
+# res = model.invoke("hi, how are you?")
+
+# print(res)
+
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+# res = model.invoke("hi, how are you?")
+
+# print(res.content)
+
