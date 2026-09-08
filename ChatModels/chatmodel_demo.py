727d09cab3e45b4bcd0133237d7b9967259a8295
kaatreHx <helloworld00ff@gmail.com>
2026-09-08 22:45:24 +0545

    chatmodel

--- ChatModels/chatmodel_demo.py
+++ ChatModels/chatmodel_demo.py
@@ -20,3 +20,17 @@
 
 # print(res.content)
 
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+res = model.invoke("what is the capital of india")
+print(res.content)
