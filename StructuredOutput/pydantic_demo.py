c8f4771dc1af9d08bfbeca51ad61234ed8f906b4
kaatreHx <helloworld00ff@gmail.com>
2026-09-14 22:29:02 +0545

    learning pydantic

---
+++ StructuredOutput/pydantic_demo.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+    name: str
+    age: int
+    email: Optional[EmailStr] = None
+    grade: int = Field(ge=1, le=10, description='Student Grade')
+    address: str = Field(default="Kathmandu")
+
+
+new_student = {
+    "name": "Manish",
+    "age": 22,
+    "email": "manish@gmail.com",
+    "grade": 9,
+    "address": "Kathmandu"
+    }
+
+student = Student(**new_student)
+
+student_dict = student.model_dump()
+
+print(student_dict)
+
+student_json = student.model_dump_json()
+
+print(student_json)
